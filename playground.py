58f36a6eb8718ffa9b6098a272a9f762a5d550e9
HwanGonJang <myggona@gmail.com>
2024-08-07 09:42:06 +0900

    feat: 20240807

--- playground.py
+++ playground.py
@@ -7,3 +7,7 @@ c = [[n] * (n + 1)]
 print(a)
 print(b)
 print(c)
+
+html = "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>"
+print(html.index("<html"))
+print(html.find("<html"))
